e7ffe3b98a9ac29d1fb222de6221fb9bd140a747
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-02-27 15:47:03 +0600

    for loop pra

---
+++ forLoop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+        printf("Hlow\n");
+    }
+    return 0;
+}
--- pra.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hlw C");
-    return 0; 
-}
