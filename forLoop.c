5822a4e45d92bd63f4e2bdc5aadd5a10442c38f7
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-02-28 12:14:09 +0600

    for loop update

--- forLoop.c
+++ forLoop.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 
-#include <stdio.h>
-
 int main() {
     printf("Outside of loop\n");
-    for (int i = 0; i < 3; i++) {
-        printf("Inside of loop\n");
-        printf("i = %d\n", i);
+
+    // int n = 10;
+    // for(int i = n; i > 0; i--) {
+    //     printf("i = %d\n", i);
+    // }
+
+    // for (int i = 0; i < 3; i++) {
+    //     printf("Inside of loop\n");
+    //     printf("i = %d\n", i);
+    // }
+
+    for (int i = 1; i <= 10; i++) {
+        if(i % 2 == 0) {
+            printf("%d Even\n", i);
+        } else {
+            printf("%d Odd\n", i);
+        }
     }
+
+    // for(char i = 'a'; i <= 'z'; i++) {
+    //     printf("%c = %d\n", i, i);
+    //     // print asci table numbers of characters.
+    // }
+
     printf("Outside of looop\n");
+
     return 0;
 }
