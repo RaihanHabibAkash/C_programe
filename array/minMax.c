81ced303bce888a1ea7e3ab2bd849dcde3761fa3
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-03-04 19:15:50 +0600

    min max created

---
+++ array/minMax.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int n;
+    scanf(" %d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int min = INT_MAX,
+        max = INT_MIN;
+    
+    for(int j = 0; j < n; j++) {
+        if(arr[j] < min) {
+            min = arr[j];
+        }
+        if(arr[j] > max) {
+            max = arr[j];
+        }
+    }
+
+    printf("min = %d\nmax = %d\n", min, max);
+
+    
+    return 0;
+}
