e0bf1bdc12a2a1743dc35f3671ebacaa24138bae
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-03-08 12:24:21 +0600

    updated

--- array/projects/swapMinMax.c
+++ array/projects/swapMinMax.c
@@ -33,21 +33,19 @@ int main() {
     scanf(" %d", &n);
     int arr[n];
 
+    int minIndex = 0;
+    int maxIndex = 0;
+
     // Inserting and checking for min and max
     for(int i = 0; i < n; i++) {
         scanf(" %d", &arr[i]);
-    }
 
-    int minIndex = 0;
-    int maxIndex = 0;
-    // Checking for min max
-    for(int i = 1; i < n; i++) {
         // for min
-        if(arr[i] < arr[minIndex]) {
+        if(arr[i] <= arr[minIndex]) {
             minIndex = i;
         }
         // for max
-        if(arr[i] > arr[maxIndex]) {
+        if(arr[i] >= arr[maxIndex]) {
             maxIndex = i;
         }
     }
