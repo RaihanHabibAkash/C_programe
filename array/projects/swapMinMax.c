f32e2f22036405def587e16ddf05ec26995917cc
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-03-08 12:53:49 +0600

    Lucky arrray. the arr min freq is even or odd

--- array/projects/swapMinMax.c
+++ array/projects/swapMinMax.c
@@ -33,19 +33,19 @@ int main() {
     scanf(" %d", &n);
     int arr[n];
 
-    int minIndex = 0;
-    int maxIndex = 0;
+    int minIndex = 0,
+        maxIndex = 0;
 
     // Inserting and checking for min and max
     for(int i = 0; i < n; i++) {
         scanf(" %d", &arr[i]);
 
         // for min
-        if(arr[i] <= arr[minIndex]) {
+        if(arr[i] < arr[minIndex]) {
             minIndex = i;
         }
         // for max
-        if(arr[i] >= arr[maxIndex]) {
+        if(arr[i] > arr[maxIndex]) {
             maxIndex = i;
         }
     }
