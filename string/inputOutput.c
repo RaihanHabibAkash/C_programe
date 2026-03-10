58876251d9a6b70149fe55b1855705618d4bfb59
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-03-10 13:00:53 +0600

    basic c programe string

--- string/inputOutput.c
+++ string/inputOutput.c
@@ -1,27 +1,4 @@
 // #include <stdio.h>
-
-// int main() {
-//     // char str[10] = {'A', 'p', 'p', 'l', 'e', '\0'};
-//     char str[10] = "AB CD";
-
-//     for(int i = 0; i < 5; i++) {
-//         printf("%c", str[i]);
-//     }
-//     return 0;
-// }
-
-
-
-// #include <stdio.h>
-// int main() {
-//     char str[10] = "AB CD";
-//     // s = string
-//     printf("%s", str);
-//     return 0;
-// }
-
-
-// #include <stdio.h>
 // int main() {
 //     char str[20];
 
