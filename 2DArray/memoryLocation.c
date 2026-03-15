b8dd5875a5d26e937ccc5932a9559948fb10fbd2
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-03-15 16:39:32 +0600

    Memory locatons of 2D array

---
+++ 2DArray/memoryLocation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int arr2D[3][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}};
+
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 5; j++) {
+            printf("i = %d, j = %d, address = %d, value = %d\n", i, j, &arr2D[i][j], arr2D[i][j]);
+        }
+        printf("\n");
+    }
+}
