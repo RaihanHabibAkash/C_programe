b3221d59378f8ae88459a237162704b04cb92d41
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-03-17 16:46:11 +0600

    swaping using pointers

---
+++ pointers/projects/swap.c
@@ -0,0 +1,44 @@
+/*
+E. Swap
+time limit per test1 second
+memory limit per test64 megabytes
+Given two numbers X and Y. Print X and Y after swapping them.
+
+Note: Solve this problem using function.
+
+Input
+Only one line contains two numbers X and Y (0 ≤ X, Y ≤ 105).
+
+Output
+Print X and Y separated by a space after swapping.
+
+Example
+InputCopy
+5 2
+OutputCopy
+2 5
+*/
+
+// https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/E
+
+
+#include <stdio.h>
+
+int main() {
+    // Taking inputs
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    // Pointers
+    int *x = &a,
+        *y = &b;
+
+    // Swaping 
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+
+    printf("%d %d", a, b);
+    
+    return 0;
+}
