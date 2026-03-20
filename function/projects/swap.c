23967f7a9336829fef743dc676e78dc1248096fd
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-03-20 08:42:06 +0600

    swap using refrence

---
+++ function/projects/swap.c
@@ -0,0 +1,43 @@
+/*
+E. Swap
+time limit per test1 second
+memory limit per test64 megabytes
+Given two numbers X and Y. Print X and Y after swapping them.
+
+Note: Solve this problem using function.
+
+Input
+Only one line contains two numbers X and Y (0 ≤ X, Y ≤ 105).
+
+Output
+Print X and Y separated by a space after swapping.
+
+Example
+InputCopy
+5 2
+OutputCopy
+2 5
+*/
+
+// https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/E
+
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+    // Taking inputs
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    // Sending refence of a, b
+    swap(&a, &b);
+
+    printf("%d %d", a, b);
+
+    return 0;
+}
